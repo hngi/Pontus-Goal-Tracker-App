5bb7c0ec82f409ebe6773505a8f0a193709dbf51
Noel-elon <noelnwaelugo@gmail.com>
2019-09-24 15:40:19 +0100

    completed the major sign In process

--- app/src/main/java/com/project/pontusgoaltracker/SignIn.java
+++ app/src/main/java/com/project/pontusgoaltracker/SignIn.java
@@ -3,35 +3,41 @@ package com.project.pontusgoaltracker;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SignIn extends AppCompatActivity implements View.OnClickListener {
 
+
     EditText EmailET, PwdET;
     CheckBox remember;
     Button button;
     TextView signUP;
     ImageView google, linked, facebook;
-
-import android.widget.TextView;
-
-public class SignIn extends AppCompatActivity {
-    TextView sign_up;
-
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +48,8 @@ public class SignIn extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
+        //Initialize all views by ID
+        mAuth = FirebaseAuth.getInstance();
         EmailET = findViewById(R.id.email);
         PwdET = findViewById(R.id.password);
         remember = findViewById(R.id.checkBox);
@@ -51,6 +58,13 @@ public class SignIn extends AppCompatActivity {
         google = findViewById(R.id.google);
         linked = findViewById(R.id.ln);
         facebook = findViewById(R.id.fb);
+        progressBar = findViewById(R.id.progressBar2);
+
+
+        progressBar.setVisibility(View.GONE);
+
+
+        //set onclick listeners where necessary
 
         button.setOnClickListener(this);
         signUP.setOnClickListener(this);
@@ -59,25 +73,60 @@ public class SignIn extends AppCompatActivity {
         facebook.setOnClickListener(this);
 
 
-        sign_up = findViewById(R.id.sign);
-        sign_up.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignIn.this, SignUp.class);
-                startActivity(intent);
-            }
-        });
-
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
+            //If the sign in button is clicked
             case R.id.button:
+//Get the texts from the edittexts
+                String LoginEmail = EmailET.getText().toString();
+                String LoginPassword = PwdET.getText().toString();
+
+                //Validation
+                if (LoginEmail.isEmpty()) {
+                    EmailET.setError("Please enter Email");
+                    EmailET.requestFocus();
+                } else if (LoginPassword.isEmpty()) {
+                    PwdET.setError("Enter your Password Please");
+                    PwdET.requestFocus();
+//Check if email matches normal pattern
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(LoginEmail).matches()) {
+                    EmailET.setError("Please enter a valid email address");
+                    EmailET.requestFocus();
+                    return;
+                    //if the texts boxes are empty
+                } else if (TextUtils.isEmpty(LoginEmail) && TextUtils.isEmpty(LoginPassword)) {
+
+                    Toast.makeText(SignIn.this, "Please fill the Boxes", Toast.LENGTH_SHORT).show();
+
+                    //if they are not empty, Login
+                } else if (!(TextUtils.isEmpty(LoginEmail) && TextUtils.isEmpty(LoginPassword))) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    mAuth.signInWithEmailAndPassword(LoginEmail, LoginPassword).addOnCompleteListener(SignIn.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            //if login is successful
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(SignIn.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            } else {
+                                progressBar.setVisibility(View.GONE);
+                                Toast.makeText(SignIn.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                //Proceed to next activity
+                            }
+
+                        }
+                    });
+                } else {
+                    Toast.makeText(SignIn.this, "Error Occured!! ", Toast.LENGTH_SHORT).show();
+                }
+
 
                 break;
             case R.id.sign:
-                Intent i = new Intent(SignIn.this , SignUp.class  );
+                Intent i = new Intent(SignIn.this, SignUp.class);
                 startActivity(i);
 
                 break;
