ab9d753c94a235a78a9ffc0189094113e2ae2834
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2019-09-24 21:19:14 +0100

    Set timer for the main activity

--- app/src/main/java/com/project/pontusgoaltracker/MainActivity.java
+++ app/src/main/java/com/project/pontusgoaltracker/MainActivity.java
@@ -2,7 +2,11 @@ package com.project.pontusgoaltracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Makes the activity visible for 5 seconds //
+        int Timeout = 5000;
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                Intent intent = new Intent(MainActivity.this, SignIn.class);
+                startActivity(intent);
+            }
+        }, Timeout);
     }
 }
