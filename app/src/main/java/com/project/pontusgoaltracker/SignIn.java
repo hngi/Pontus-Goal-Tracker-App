596c219ed3aa6714f2ae965e2cd919fcb76ce296
Noel-elon <noelnwaelugo@gmail.com>
2019-09-24 14:16:30 +0100

    initialized all buttons and edit texts

--- app/src/main/java/com/project/pontusgoaltracker/SignIn.java
+++ app/src/main/java/com/project/pontusgoaltracker/SignIn.java
@@ -1,5 +1,6 @@
 package com.project.pontusgoaltracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,8 +10,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignIn extends AppCompatActivity {
+public class SignIn extends AppCompatActivity implements View.OnClickListener {
+
+    EditText EmailET, PwdET;
+    CheckBox remember;
+    Button button;
+    TextView signUP;
+    ImageView google, linked, facebook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,50 @@ public class SignIn extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        EmailET = findViewById(R.id.email);
+        PwdET = findViewById(R.id.password);
+        remember = findViewById(R.id.checkBox);
+        button = findViewById(R.id.button);
+        signUP = findViewById(R.id.sign);
+        google = findViewById(R.id.google);
+        linked = findViewById(R.id.ln);
+        facebook = findViewById(R.id.fb);
+
+        button.setOnClickListener(this);
+        signUP.setOnClickListener(this);
+        google.setOnClickListener(this);
+        linked.setOnClickListener(this);
+        facebook.setOnClickListener(this);
+
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+
+                break;
+            case R.id.sign:
+                Intent i = new Intent(SignIn.this , SignUp.class  );
+                startActivity(i);
+
+                break;
+            case R.id.google:
+                Toast.makeText(SignIn.this, "Feature under construction", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ln:
+                Toast.makeText(SignIn.this, "Feature under construction", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.fb:
+                Toast.makeText(SignIn.this, "Feature under construction", Toast.LENGTH_SHORT).show();
+                break;
+
+
+        }
+
+    }
 }
