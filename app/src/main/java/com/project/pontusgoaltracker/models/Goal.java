af2d4bb7ccdddb6a784d7547793c482f30f438a7
joshbillionz <joshuaerondu4@gmail.com>
2019-09-26 10:52:55 +0100

    JoshBillionz commit1 1 i added/updated User,Task,Goal, GoalType classes 2 i added firebaseDatabase dependency to the app. 3 i added drawable-xxhdpi folder and copied into it files from drawable folder to support devices like mine with xx highd density

--- app/src/main/java/com/project/pontusgoaltracker/models/Goal.java
+++ app/src/main/java/com/project/pontusgoaltracker/models/Goal.java
@@ -1,4 +1,131 @@
 package com.project.pontusgoaltracker.models;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
+
 public class Goal {
+
+    private UUID goalId ;
+    private String title;
+    private String description;
+    private Date dateCreated;
+    private Date deadline;
+    private Date dateCompleted;
+    private boolean isCompleted;
+    private String goalType;
+    private ArrayList<Task> tasks;
+
+    //todo : add id to goal
+    //todo : create setDeadline method
+    //todo : add deadline to the Goal
+    //todo : add percentageComplete method
+
+
+    //constructors
+    //warning : do not use this constructor. do not delete either .
+    //No constructor sets the deadline of Goal.. explicitly call setDeadline after creating an object.
+    public Goal(){}
+
+    public Goal(String title){
+        this.dateCreated = new Date();
+        this.goalId = UUID.randomUUID();
+        this.title = title;
+        this.tasks = new ArrayList<Task>();
+    }
+
+    public Goal(String title, String description) {
+        this.dateCreated = new Date();
+        this.goalId = UUID.randomUUID();
+        this.title = title;
+        this.description = description;
+        this.goalType = GoalType.GENERAL;
+        this.tasks = new ArrayList<Task>();
+    }
+
+    public Goal(String title, String description, String type) {
+        this.dateCreated = new Date();
+        this.goalId = UUID.randomUUID();
+        this.title = title;
+        this.description = description;
+        this.goalType = type;
+        this.tasks = new ArrayList<Task>();
+    }
+
+    public Goal(String title, String description, String type, ArrayList<Task> tasks) {
+        this.dateCreated = new Date();
+        this.goalId = UUID.randomUUID();
+        this.title = title;
+        this.description = description;
+        this.goalType = type;
+        this.tasks = tasks;
+    }
+
+    //setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDateCompleted(Date dateCompleted) {
+        this.dateCompleted = dateCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public void setType(String type) {
+        this.goalType = type;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+
+    //getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public Date getDateCompleted() {
+        return dateCompleted;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public String getType() {
+        return goalType;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    //append
+
+    public void addTask(Task task){
+        tasks.add(task);
+    }
+
 }
+
