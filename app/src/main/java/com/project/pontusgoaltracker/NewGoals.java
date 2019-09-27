57ae904d4d732fd072b587ae7fe0a38a22247ed4
Noel-elon <noelnwaelugo@gmail.com>
2019-09-27 09:10:56 +0100

    Implemented the add tasks feature

--- app/src/main/java/com/project/pontusgoaltracker/NewGoals.java
+++ app/src/main/java/com/project/pontusgoaltracker/NewGoals.java
@@ -1,17 +1,25 @@
 package com.project.pontusgoaltracker;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -30,6 +38,12 @@ public class NewGoals extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_times_solid);
+
+        final EditText edittext = new EditText(this);
+        edittext.setPaddingRelative(8,8,8,8);
+
+        //Array that holds names for the list
+        final ArrayList<String> items = new ArrayList<>();
         //Populating the task Spinner
         List<String> spinnerArray = new ArrayList<String>();
         spinnerArray.add("Goal Type");
@@ -44,6 +58,97 @@ public class NewGoals extends AppCompatActivity {
         myCalendar = Calendar.getInstance();
         Dates = findViewById(R.id.date);
 
+        final ListView list = findViewById(R.id.task);
+
+        //Setting the adapter for list view
+        final ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, R.layout.task_menu, R.id.checkedTextView, items);
+        list.setAdapter(listAdapter);
+
+        //First Alert Dialogue for FAB
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setMessage("New Task");
+        alert.setTitle("Enter New Task");
+
+        alert.setView(edittext);
+
+        alert.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String newTask = edittext.getText().toString();
+                items.add(newTask);
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.cancel();
+            }
+        });
+
+        //Second Alert Dialogue for Deleting
+
+        final AlertDialog.Builder alert2 = new AlertDialog.Builder(this);
+        alert2.setMessage("Delete Task?");
+        alert2.setTitle("Delete");
+
+
+        alert2.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.cancel();
+            }
+        });
+
+
+//On click for list items
+        list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = ((TextView) view).getText().toString();
+                Toast.makeText(NewGoals.this, selectedItem, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //on long click for deleting items
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int i, long l) {
+                alert2.setMessage("Delete Task?");
+                alert2.setTitle("Delete");
+
+
+                alert2.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.cancel();
+                    }
+                });
+
+                alert2.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+
+                        items.remove(i);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                alert2.show();
+
+                return true;
+            }
+        });
+
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext.getParent() != null) {
+                    ((ViewGroup) edittext.getParent()).removeView(edittext);
+                    edittext.setText("");//
+                }
+                alert.show();
+            }
+        });
 
         // Creating a Date Picker Dialog
         DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
