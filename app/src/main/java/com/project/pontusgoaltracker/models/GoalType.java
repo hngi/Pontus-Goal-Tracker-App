4933cae1498033eef4356432c5a731e595729841
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2019-09-27 23:02:07 +0100

    Create GoalType.java

---
+++ app/src/main/java/com/project/pontusgoaltracker/models/GoalType.java
@@ -0,0 +1,30 @@
+package com.project.pontusgoaltracker.models;
+
+import java.util.ArrayList;
+
+public abstract class GoalType{
+
+		//note : dont try to make objects from this class
+
+        //todo : allow users to create custom Goal types:
+        //todo : let each Goal type have customisable properties like colour;
+
+
+		final public static String NULL = null;
+        final public static String GENERAL = "General";
+        final public static String RELIGIOUS ="Religious";
+        final public static String FINANCIAL = "Financial";
+        final public static String HEALTH ="Health";
+        final public static String RELATIONSHIP = "Relationship";
+        final public static String FAMILY ="Family";
+        final public static String OCCUPATIONAL = "Occupational";
+        final public static String ACADEMIC ="Academic";
+        final public static String PERSONAL ="Personal";
+
+        final public static String[] allGoalTypes={NULL, GENERAL,RELIGIOUS,FINANCIAL,HEALTH,
+        RELATIONSHIP,FAMILY,OCCUPATIONAL,ACADEMIC,PERSONAL};
+
+
+
+
+    }
