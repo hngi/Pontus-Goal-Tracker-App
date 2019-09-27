aa22efd483cd4f2c6a1c528c13d0658018deeee7
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2019-09-27 23:00:47 +0100

    Create DashboardActivity.java

---
+++ app/src/main/java/com/project/pontusgoaltracker/DashboardActivity.java
@@ -0,0 +1,14 @@
+package com.project.pontusgoaltracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
